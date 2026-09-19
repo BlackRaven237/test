84c95b0c0dacd7641adef2529f73f851f07ec66a
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-19 17:03:31 +0100

    feat: Implementing multiply(a, b) and divide(a, b) in math.cpp

--- math.cpp
+++ math.cpp
@@ -2,3 +2,8 @@
 
 int add(int a, int b) { return a + b; }
 int substract(int a, int b) { return a + b; }
+int multiply(int a, int b) { return a * b; }
+int divide(int a, int b) {
+    if (b == 0) return;
+    return a / b;
+}
