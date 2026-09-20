645bf4a10c27a7b4a38bcfe6fc26625945462b51
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-20 05:03:40 +0100

    Changing add(5, 7) -> add(5, 10)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main () {
-    std::cout << add(5, 7) << std::endl;
+    std::cout << add(5, 10) << std::endl;
     std::cout << substract(5, 7) << std::endl;
     std::cout << multiply(5, 7) << std::endl;
     std::cout << divide(5, 7) << std::endl;
