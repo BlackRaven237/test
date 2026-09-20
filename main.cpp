e1b9b0cade08fde032ee5affc1b2c97a8b579bfa
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-20 06:39:11 +0100

    Changing substract(5, 7) -> substract(15, 6)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main () {
     std::cout << add(5, 10) << std::endl;
-    std::cout << substract(5, 7) << std::endl;
+    std::cout << substract(15, 6) << std::endl;
     std::cout << multiply(5, 7) << std::endl;
     std::cout << divide(5, 7) << std::endl;
     return 0;
