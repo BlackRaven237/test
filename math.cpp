4eb1dddd2059d497dd26b337e6625cb45753c4e6
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-20 16:39:45 +0100

    Changing implememtation to a - b

--- math.cpp
+++ math.cpp
@@ -1,7 +1,7 @@
 #include "math.h"
 
 int add(int a, int b) { return a + b; }
-int substract(int a, int b) { return a + b; }
+int substract(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divide(int a, int b) {
     if (b == 0) return 0;
